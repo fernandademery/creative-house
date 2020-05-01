468db4d89722c0adc207f1d9da65a98e7e21dd9d
Fernanda D'Emery <nandademery2@hotmail.com>
2020-05-01 13:35:36 +0200

    Starting backend

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const server = express();
+
+// Configure static files
+server.use(express.static("public"));
+
+server.get("/", function (req, res) {
+  return res.sendFile(__dirname + "/index.html");
+});
+
+server.get("/ideas", function (req, res) {
+  return res.sendFile(__dirname + "/ideas.html");
+});
+
+server.listen(3000);
