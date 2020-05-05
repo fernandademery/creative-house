454e38d481e20844452a68247061baa59f6d78ac
Fernanda D'Emery <nandademery2@hotmail.com>
2020-05-05 19:07:12 +0200

    Building database with sqlite3

---
+++ db.js
@@ -0,0 +1,37 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./workshopdev.db");
+
+db.serialize(function () {
+    // Create table
+    db.run(`CREATE TABLE IF NOT EXISTS ideas(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    image TEXT,
+    title TEXT,
+    category TEXT,
+    desciption TEXT,
+    url TEXT
+)`);
+    // Insert data into table
+    const query = `
+    INSERT INTO ideas(
+        image, title, category, desciption, url
+    ) VALUES (?, ?, ?, ?, ?);
+    `
+
+    const values = [
+        "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        "Coding Courses",
+        "Learning",
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo neque nam, tempore perspiciatis.",
+        "https://rocketseat.com.br"
+    ];
+
+    // db.run(query, values, function (err) {
+    //     if (err) return console.log(err)
+    //     console.log(this)
+    // });
+
+    // Get data from table
+
+    // Delete data from table 
+});
